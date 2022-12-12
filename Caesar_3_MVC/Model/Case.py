43a707e9a693ffec61dcc43aa21b48ca7596db65
Joshua <jguyot>
2022-12-12 15:49:49 +0100

    Ajout des colisions en fonction du sprite sur les cases

--- Caesar_3_MVC/Model/Case.py
+++ Caesar_3_MVC/Model/Case.py
@@ -1,3 +1,5 @@
+from Model.constants import list_of_collision
+
 class Case():
     def __init__(self, x, y, rectangle_cell, isometric_cell, render_pos, sprite=None):
         self.x = x
@@ -6,6 +8,10 @@ class Case():
         self.isometric_cell = isometric_cell
         self.render_pos = render_pos
         self.sprite = sprite
+        if (self.sprite in list_of_collision ):
+            self.collision = 1
+        else:
+            self.collision = 0 
 
 class Route(Case):
     def __init__(self, x, y, rectangle_cell, isometric_cell, render_pos, sprite):
