b353d2e559047775a28f2e376e393cf84f845da3
Pxtchi <guyot.joshua@gmail.com>
2023-01-10 15:56:10 +0100

    REmise en fonction des boutons overlay

--- Caesar_3_MVC/View/view.py
+++ Caesar_3_MVC/View/view.py
@@ -57,7 +57,7 @@ class GraphicalView(object):
                 self.renderMenu()
             elif currentstate == STATE_PLAY:
                 self.renderGame()
-            self.clock.tick(60)
+            self.clock.tick(120)
 
 
     def renderIntroScene(self) -> None:
@@ -84,7 +84,7 @@ class GraphicalView(object):
             self.initialize()
         self.model.actualGame.update()
         self.model.actualGame.draw()
-        self.model.mini_map.draw_position(self.model.actualGame.screen, self.model.actualGame.camera,self.model.actualGame.map)
+        # self.model.mini_map.draw_position(self.model.actualGame.screen, self.model.actualGame.camera,self.model.actualGame.map)
         self.model.pause_menu.draw_pause_menu()
         self.model.actualGame.restart = False
         pygame.display.flip()
