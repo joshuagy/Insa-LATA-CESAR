89737daf2daf4305b4a12f132c869a9dc4675e76
TomhhHHHH <tom_hodemon@outlook.fr>
2023-01-10 13:01:05 +0100

    default effect

--- Caesar_3_MVC/Model/Plateau.py
+++ Caesar_3_MVC/Model/Plateau.py
@@ -356,40 +356,44 @@ class Plateau():
         for cell_x in range(self.nbr_cell_y):
             for cell_y in range(self.nbr_cell_y):
                 render_pos =  self.map[cell_x][cell_y].render_pos
-                id_image = self.map[cell_x][cell_y].sprite
-
+                id_image = None
+                image = None
                 # DRAW DEFAULT CELLS
                 if not self.map[cell_x][cell_y].road and not self.map[cell_x][cell_y].structure:
                     id_image = self.map[cell_x][cell_y].sprite
-                    self.screen.blit(self.image[id_image],
+                    image = self.image[id_image]
+                    self.screen.blit(image,
                                     (render_pos[0] + self.surface_cells.get_width()/2 + self.camera.vect.x,
-                                    render_pos[1] - (self.image[id_image].get_height() - cell_size) + self.camera.vect.y))
+                                    render_pos[1] - (image.get_height() - cell_size) + self.camera.vect.y))
                 # DRAW ROADS
                 elif self.map[cell_x][cell_y].road:
                     id_image = self.map[cell_x][cell_y].road.sprite
-                    self.screen.blit(self.image_route[id_image],
+                    image = self.image_route[id_image]
+                    self.screen.blit(image,
                                     (render_pos[0] + self.surface_cells.get_width()/2 + self.camera.vect.x,
-                                    render_pos[1] - (self.image_route[id_image].get_height() - cell_size) + self.camera.vect.y))
+                                    render_pos[1] - (image.get_height() - cell_size) + self.camera.vect.y))
 
                 # DRAW STRUCTURES
                 elif isinstance(self.map[cell_x][cell_y].structure, BurningBuilding):
+                    image = self.image_structures["BurningBuilding"][int(self.map[cell_x][cell_y].structure.index_sprite)]
                     self.screen.blit(self.image_structures["BurningBuilding"][int(self.map[cell_x][cell_y].structure.index_sprite)], 
                                     (render_pos[0] + self.surface_cells.get_width()/2 + self.camera.vect.x,
                                         render_pos[1] - (self.image_structures["BurningBuilding"][int(self.map[cell_x][cell_y].structure.index_sprite)].get_height() - cell_size) + self.camera.vect.y))
                                         
                 elif self.map[cell_x][cell_y].structure.case == self.map[cell_x][cell_y] :
                     id_image = self.map[cell_x][cell_y].structure.desc
-                    self.screen.blit(self.image_structures[id_image], 
+                    image = self.image_structures[id_image]
+                    self.screen.blit(image, 
                                         (render_pos[0] + self.surface_cells.get_width()/2 + self.camera.vect.x,
-                                            render_pos[1] - (self.image_structures[id_image].get_height() - cell_size) + self.camera.vect.y))
+                                            render_pos[1] - (image.get_height() - cell_size) + self.camera.vect.y))
 
                 # DRAW PREVIEWED CELLS AND HOVERED CELLS
                 if self.foreground.hasEffect(cell_x, cell_y):
-                    id_image = self.map[cell_x][cell_y].sprite
-                    effectedImage = self.foreground.getEffectedImage(self.image[id_image].copy(), cell_x, cell_y)
+                    # id_image = self.map[cell_x][cell_y].sprite
+                    effectedImage = self.foreground.getEffectedImage(image.copy(), cell_x, cell_y)
                     self.screen.blit(effectedImage,
                                     (render_pos[0] + self.surface_cells.get_width()/2 + self.camera.vect.x,
-                                    render_pos[1] - (self.image[id_image].get_height() - cell_size) + self.camera.vect.y))
+                                    render_pos[1] - (image.get_height() - cell_size) + self.camera.vect.y))
 
                 # DRAW WALKERS
                 for e in self.walkers[cell_x][cell_y]:
