7beafc0e5a4bde54b7d0e77791d2e8fc77e36f60
Yann-j3 <gaudichaudyann@gmail.com>
2023-04-11 17:09:32 +0200

    Cost Colosseum

--- Model/constants.py
+++ Model/constants.py
@@ -36,7 +36,7 @@ GRANARY_COST = 100
 WHEATFARM_COST = 50
 TEMPLE_COST = 300
 SENATE_COST = 500
-COLOSSEUM_COST = 1000
+COLOSSEUM_COST = 4000
 
 
 list_of_collision = ["tree", "water", "rock", "sign"]
